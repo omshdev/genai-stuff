4f2673289200519287f963da958c35470ea164fc
Om Sharma <omsharma.83173@gmail.com>
2025-09-01 17:49:13 +0530

    topic : message_placeholders.

--- 2.Langchain/1.LLM/src/mini-projects/3.stateful-chatbot-using-langchain-messages-history.ts
+++ 2.Langchain/1.LLM/src/mini-projects/3.stateful-chatbot-using-langchain-messages-history.ts
@@ -1,14 +1,11 @@
 import {  HumanMessage,SystemMessage,AIMessage} from "@langchain/core/messages";
 import { ChatGoogleGenerativeAI} from "@langchain/google-genai";
-import { ChatPromptTemplate , MessagesPlaceholder} from "@langchain/core/prompts"
-
+import { ChatPromptTemplate } from "@langchain/core/prompts"
 import dotenv from "dotenv"
 dotenv.config();
 
 const SYSTEM_TEMPLTE = `
 Your're an Om Sharma's Assistant!. the User will give you input {user_input} and will respond based on query. and You Have History of Messages {chatHistory} So You Will not Loose the Context What user is Chating To You.
-
-
 `
 
 
@@ -44,16 +41,13 @@ chatHistory.push(new SystemMessage(SYSTEM_TEMPLTE));
 
 async function chat( user : string){
     const promptTemplate = await ChatPromptTemplate.fromMessages([
-    // ["system",SYSTEM_TEMPLTE], ["human","{user_input}"], ["human","{chatHistory}"]
-    ["system",SYSTEM_TEMPLTE],["human","{user_input}"],new MessagesPlaceholder("chatHistory")
+    ["system",SYSTEM_TEMPLTE], ["human","{user_input}"], ["human","{chatHistory}"]
     ]);
     
     const promptInputValue = await promptTemplate.invoke({
         user_input:user,
         chatHistory : JSON.stringify(chatHistory)
     });
-
-    console.log("keerthi",promptInputValue);
 
     const response = await model.invoke(promptInputValue);
     console.log("Bot : ",response.content);

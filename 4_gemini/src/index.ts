0e887d837af69e89b13bf4c983d2a9de4861eee0
Om Sharma <omsharma.83173@gmail.com>
2026-06-04 22:59:59 +0530

    steraming-repsonse.

--- 4_gemini/src/index.ts
+++ 4_gemini/src/index.ts
@@ -6,12 +6,24 @@ dotenv.config();
 const ai = new GoogleGenAI({apiKey : process.env.GOOGLE_GEMINI_API_KEY!});
 
 async function main(){
-    const response = await ai.models.generateContent({
+    // Generate Text
+    // const response = await ai.models.generateContent({
+    //     model : "gemini-3.5-flash",
+    //     contents : " Who is Andrej Karpathy..",
+    // });
+
+    // console.log(response.text);
+
+    // stream response for faster.
+    const responseStream = await ai.models.generateContentStream({
         model : "gemini-3.5-flash",
-        contents : " Who is Andrej Karpathy..",
+        contents : "Who is Andrej Karpathy ?"
     });
 
-    console.log(response.text);
+    for await (const chunk of responseStream){
+        // @ts-ignore
+        process.stdout.write(chunk?.text);
+    }
 }
 
 main();

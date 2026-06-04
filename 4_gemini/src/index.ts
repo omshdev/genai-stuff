43f7063fcaa5ae0c391d1097df5072cac311b731
Om Sharma <omsharma.83173@gmail.com>
2026-06-04 23:09:04 +0530

    code-execution-using-gemini-apis.

--- 4_gemini/src/index.ts
+++ 4_gemini/src/index.ts
@@ -5,25 +5,55 @@ dotenv.config();
 
 const ai = new GoogleGenAI({apiKey : process.env.GOOGLE_GEMINI_API_KEY!});
 
+// async function runTextGenerationAndStreamResponse(){
+//     // Generate Text
+//     // const response = await ai.models.generateContent({
+//     //     model : "gemini-3.5-flash",
+//     //     contents : " Who is Andrej Karpathy..",
+//     // });
+
+//     // console.log(response.text);
+
+//     // stream response for faster.
+//     const responseStream = await ai.models.generateContentStream({
+//         model : "gemini-3.5-flash",
+//         contents : "Who is Andrej Karpathy ?"
+//     });
+
+//     for await (const chunk of responseStream){
+//         // @ts-ignore
+//         process.stdout.write(chunk?.text);
+//     }
+// }
+// runTextGenerationAndStreamResponse();
+
 async function main(){
-    // Generate Text
-    // const response = await ai.models.generateContent({
-    //     model : "gemini-3.5-flash",
-    //     contents : " Who is Andrej Karpathy..",
-    // });
-
-    // console.log(response.text);
-
-    // stream response for faster.
-    const responseStream = await ai.models.generateContentStream({
+    const response = await ai.models.generateContent({
         model : "gemini-3.5-flash",
-        contents : "Who is Andrej Karpathy ?"
+        contents : [
+                "What is the sum of the first 50 prime numbers? " +
+      "Generate and run code for the calculation, and make sure you get all 50.",
+        ],
+        config:{
+            tools : [{codeExecution:{}}],
+        }
     });
-
-    for await (const chunk of responseStream){
-        // @ts-ignore
-        process.stdout.write(chunk?.text);
+    
+    const parts = response?.candidates?.[0]?.content?.parts || [];
+        
+    parts.forEach((part) => {
+        if (part.text) {
+        console.log(part.text);
     }
-}
 
-main();
+  if (part.executableCode && part.executableCode.code) {
+    console.log(part.executableCode.code);
+  }
+
+  if (part.codeExecutionResult && part.codeExecutionResult.output) {
+    console.log(part.codeExecutionResult.output);
+  }
+    
+})}
+
+main()

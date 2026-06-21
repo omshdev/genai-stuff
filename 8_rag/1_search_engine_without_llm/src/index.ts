aa08639db58fd50b4d0083908083eb92b5fa1723
Om Sharma <omsharma.83173@gmail.com>
2026-06-21 22:06:42 +0530

    feat(rag): implement chunking strategies and embedding generation

--- 8_rag/1_search_engine_without_llm/src/index.ts
+++ 8_rag/1_search_engine_without_llm/src/index.ts
@@ -1,12 +1,39 @@
-// 1. chunking decision.
-
 import {fixedSizeChunker} from "./chunkers/fixed_size_chunker.js" 
 import { markdownHeadingChunker } from "./chunkers/markdownHeadingChunker.js";
 import {paragraphChunker} from "./chunkers/paragraph_chunker.js"
-import {fifteen100} from "./text/1500.js"
+import { client,runQdrant } from "./qdrant.js";
+import { fifteen100 } from "./text/1500.js"
 import { markDown } from "./text/markDown.js";
+import { runEmbeddingModel } from "./utils/runEmbeddingModel.js";
+import { runModel } from "./utils/runModel.js";
 
 // const response = fixedSizeChunker(fifteen100,500,100);
 // const response = paragraphChunker(fifteen100);
-const response = markdownHeadingChunker(markDown);
-console.log(response);
+// const response = markdownHeadingChunker(markDown);
+// console.log(response);
+
+async function main(){
+    // first chunk the content using  diffrent chunkers and put them into diffrent collections.
+    // 1 . fixed size chunker.
+    const response:any = fixedSizeChunker(fifteen100,500,100);
+
+    const embeddings = await Promise.all(
+        response.map((chunk : any)=>runEmbeddingModel(chunk.content))
+    )
+    
+    const points = embeddings.map((chunk:any,index:number)=>({
+        id : index,
+        vector : embeddings[index].embeddings[0]
+    })
+        
+    )
+    console.log("dewd",points[0]);
+    
+    // const insertToCollection = await client.upsert("rag",{
+    //     wait : true,
+    //     points : points
+    // });
+    
+}   
+// runQdrant();
+main()
--- 8_rag/1_search_engine_without_llm/src/utils/runModel.ts
+++ 8_rag/1_search_engine_without_llm/src/utils/runModel.ts
@@ -1,27 +1,13 @@
-import { Ollama } from "ollama";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export const ollama = new Ollama({
-    host:"https://ollama.com",
-    headers : {
-        Authorization: "Bearer " + process.env.OLLAMA_API_KEY,
-    }
-});
+import { ollama } from "./ollama.js"
 
 export async function runModel(prompt : string){
     const response:any = await ollama.chat({ 
-        model : "nomic-embed-text",
+        model : "gpt-oss:120b",
         messages : [
-            { role : "user",content : "Convert to embedding..."}
+            {role : "system" , content: "YOU'RE AN HELPFULL ASSISTANT OF OM SHARMA."},
+            { role : "user",content : prompt}
         ]
     });
-    let finalEmbeddings = '';
-    for await (const chunk of response){
-        finalEmbeddings+=chunk.message.content;
-    }
-    console.log("Final EMbeeding",finalEmbeddings);
-    return finalEmbeddings;
-
+    console.log(response);        
+    return response.message.content;
 }
